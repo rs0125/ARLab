7637ebcb00b7e4e1234abbf03ff5a0dbfa7fc87e
Raghav Sharma <raghaaav5@gmail.com>
2024-10-31 17:34:46 +0530

    mediapipe implementation

--- Assets/LandmarkResultReceiver.cs
+++ Assets/LandmarkResultReceiver.cs
@@ -2,11 +2,10 @@ using Mediapipe.Tasks.Vision.HandLandmarker;
 using Mediapipe.Unity.Sample.HandLandmarkDetection;
 using UnityEngine;
 using UnityEngine.Events;
+using System.Collections.Generic;
 
-[System.Serializable] public class Eventkumar : UnityEvent<HandLandmarkerResult>
-{
-
-}
+[System.Serializable]
+public class Eventkumar : UnityEvent<HandLandmarkerResult> { }
 
 public class LandmarkResultReceiver : MonoBehaviour
 {
@@ -14,23 +13,27 @@ public class LandmarkResultReceiver : MonoBehaviour
     public static LandmarkResultReceiver Instance { get; private set; }
 
     public static Eventkumar testkumar = new Eventkumar();
+    public GameObject kumar;
+
+    public GameObject[] spheres = new GameObject[21];
+
 
     private void Awake()
     {
         if (Instance == null)
         {
             Instance = this;
+            UnityThread.initUnityThread();
         }
         else
         {
             Destroy(this);
-
         }
     }
 
     private void OnDestroy()
     {
-        if(Instance == this)
+        if (Instance == this)
         {
             Instance = null;
         }
@@ -48,9 +51,22 @@ public class LandmarkResultReceiver : MonoBehaviour
     public void ReceiveLandmarkResult(HandLandmarkerResult result)
     {
         Debug.Log(result);
-        testkumar.Invoke(result);
+        UnityThread.executeInUpdate(() =>
+        {
+            // Update the position of each predefined sphere with the corresponding landmark position
+            for (int i = 0; i < spheres.Length && i < result.handLandmarks[0].landmarks.Count; i++)
+            {
+                Vector3 landmarkPosition = new Vector3(
+                    result.handLandmarks[0].landmarks[i].x * 10,    // Scale factor for Unity's 3D space
+                    -result.handLandmarks[0].landmarks[i].y * 10,   // Invert Y for Unity's coordinate system
+                    result.handLandmarks[0].landmarks[i].z * 10     // Scale factor for depth
+                );
 
+                spheres[i].transform.position = landmarkPosition;
+            }
+        });
     }
+
 
 
 }
